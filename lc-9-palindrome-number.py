7c433b52b6a602430bee24cc24f4fb5180f126fa
Kip Lim <kiphenglim@gmail.com>
2025-01-16 18:12:52 -0500

    [lc] stabilise types

--- lc-9-palindrome-number.py
+++ lc-9-palindrome-number.py
@@ -31,7 +31,7 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        dq = deque()
+        dq: deque[int] = deque()
         while x != 0:
             digit = x % 10
             dq.appendleft(digit)
